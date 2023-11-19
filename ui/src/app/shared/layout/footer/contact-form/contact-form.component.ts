516b9ae3ab166a0a82f2f8e8c063b4fab8b68e57
bestquangminh <bestquangminh@gmail.com>
2023-11-19 10:12:15 +0700

    update

--- ui/src/app/shared/layout/footer/contact-form/contact-form.component.ts
+++ ui/src/app/shared/layout/footer/contact-form/contact-form.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Papa } from 'ngx-papaparse';
+interface ProductsServices {
+  name: string;
+  partnumber: string;
+}
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss'],
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
   captcha: string;
-  constructor() {
+  ProductService: string[] = [];
+  contactForm: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {
     this.captcha = '';
+  }
+  ngOnInit(): void {
+    this.contactForm = this._formBuilder.group({
+      name: '',
+      email: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.contactForm);
   }
   resolved(captchaResponse: string) {
     this.captcha = captchaResponse;
