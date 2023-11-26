368e3c43ad45d08485a1a0c8bfbe677e29ec5185
bestquangminh <bestquangminh@gmail.com>
2023-11-26 09:55:29 +0700

    update

--- ui/src/app/shared/services/pricing-data.service.ts
+++ ui/src/app/shared/services/pricing-data.service.ts
@@ -1,18 +1,29 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PricingDataService {
-  private selectedPricingSource = new BehaviorSubject<string>('') //will save last value and emits for subscribers
-  selectedPricing = this.selectedPricingSource.asObservable() // the subscribers only read data, cants use next() or error()
+  private selectedPricingSource = new BehaviorSubject<string>(''); //will save last value and emits for subscribers
+  selectedPricing = this.selectedPricingSource.asObservable(); // the subscribers only read data, cants use next() or error()
+  private selectedProServiceNameSource = new BehaviorSubject<string>(''); // to save the selected pro service name
+  selectedProServiceName = this.selectedProServiceNameSource.asObservable();
 
+  private selectedPartNumberSource = new BehaviorSubject<string>(''); // to save the selected part number
+  selectedPartNumber = this.selectedPartNumberSource.asObservable();
 
-  constructor() {
-  }
+  constructor() {}
 
   updateSelectedPricing(price: string) {
     this.selectedPricingSource.next(price); //will store the price here
+  }
+
+  updateSelectedProServices(proService: {
+    name: string;
+    partNumber: string;
+  }): void {
+    this.selectedProServiceNameSource.next(proService.name);
+    this.selectedPartNumberSource.next(proService.partNumber);
   }
 }
