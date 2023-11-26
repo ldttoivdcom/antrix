2956e2b78c6eddc73af1876c4ce5fa8bfd213992
haitran <minhhaitrandang@gmail.com>
2023-11-26 16:23:06 +0700

    update env, rm database

--- BEAntrix/src/services/mailService.js
+++ BEAntrix/src/services/mailService.js
@@ -1,21 +1,33 @@
 const nodeMailer = require("nodemailer");
 const mailTemplate = require("../template/mailTemplate");
-require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "./config.env",
+});
+
+
+
 
 exports.sendMail = async (reqData) => {
+    const { firstName, lastName, email, prodService, phone, company, jobTitle, companyWeb, message, pricing, partNo } = reqData;
+    const thanksTitle = `Thank you for submitting the form.`;
+    const topContent = `Thank you for reaching out to us. We appreciate your interest in Antrix. Your inquiry about Product / Service: ${prodService || ""} is important to us, and we will make every effort to respond as quickly as possible. `
+    const bottomContent = `Our team will review your message and get back to you shortly. If your
+                          inquiry is urgent, please feel free to contact us directly at ${email || ""}.`
+    const ps = `Thank you for considering Antrix. We look forward to assisting you.`
     const transporter = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
+        host: process.env.SENDER_HOST,
+        port: process.env.SENDER_HOST_PORT,
         auth: {
-            user: 'no-reply@neoprototype.ca',
-            pass: 'hdhk fbhe ltah vcmv'
+            user: process.env.SENDER,
+            pass: process.env.SENDER_PASSWORD
         }
     });
         await transporter.sendMail({
-        from: 'no-reply@neoprototype.ca',
+        from:  process.env.SENDER,
         to: `${reqData.email}`,
-        subject: 'Thank You for Contacting Antrix',
-        html: mailTemplate.mailTemplate(reqData)
+        subject:  process.env.SENDER_SUBJECT,
+        html: mailTemplate.mailTemplate(thanksTitle,topContent, bottomContent, ps, reqData)
         });
 
 }
