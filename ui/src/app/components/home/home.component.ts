6bd131de8fbbc0772e1002c383a0a32065e41e43
bestquangminh <bestquangminh@gmail.com>
2023-12-02 13:53:01 +0700

    fix bug

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -34,32 +34,32 @@ export class HomeComponent implements OnInit {
     {
       header: 'OUR SERVICES',
       description:
-        'Antrix expert regulatory , clinical, and quality assurance services include',
+        'Antrix offers expert regulatory, clinical, and quality assurance services which includes',
       list: [
-        'Setup ISO 13485, ISO 9001, ISO 15189, ISO 27001, MDSAP, EU MDR 745/2017, EU IVDR 746/2017, CLIA, CAP, NYSDOH, ISO 22716, ISO 22000, CFR 210, CFR 211, FDA CFR Part 630 Quality Management System',
-        'Product Regulatory Strategy and Submissions, Product Design, Development, Risk Analysis as per ISO 14971, Usability Studies as per ISO 62366, Software Compliance as per ISO 62304, Cybersecurity, Clinical Study, Verification and Validation',
-        'Product Post Market Surveillance, Vigilance, Complaints, and Field Actions',
+        'Setup a Quality Management System and Procedures for ISO 13485, ISO 9001, ISO 15189, ISO 27001, MDSAP, EU MDR 2017/745, EU IVDR 2017/746, CLIA, CAP, NYSDOH, ISO 22716, ISO 22000, NSF-2, NSF-3, FDA CFR Part 210, 211, 600',
+        'Develop a comprehensive product regulatory strategy and submissions plan, ensuring compliance with EU, FDA, ROW and ISO 14971 risk analysis, ISO 62366 usability studies, ISO 62304 software compliance, Analytical and Clinical Study',
+        'Develop Product Post Market Surveillance, Vigilance, Complaints, and Field Actions Process and Procedures',
       ],
       link: '/info/services',
     },
     {
       header: 'OUR PRODUCTS',
       description:
-        'Antrix sells mission critical products for Medical Device, IVD, LDT, SaMD, Digital Health compliance.',
+        'Antrix sells ready to implement QMS product solutions which includes',
       list: [
-        'Quality Manual and Quality System Procedures for FDA QSR 820, ISO 13485, ISO 9001, ISO 15189, ISO 27001, MDSAP, EU MDR 745/2017, EU IVDR 746/2017, CLIA, CAP, NYSDOH, ISO 22716, ISO 22000, ISO 62366, ISO 62304, FDA CFR Part 210, FDA CFR Part 211, FDA CFR Part 630',
-        'Product Regulatory Strategy and Submissions, Product Design, Development, Risk Analysis, Cybersecurity, Clinical Study, Verification and Validation',
-        'Product Post Market Surveillance, Vigilance, Complaints, and Field Actions',
+        'Quality Manual, Procedures, Work Instructions, and Forms for ISO 13485, ISO 9001, ISO 15189, ISO 27001, MDSAP, EU MDR 2017/745, EU IVDR 2017/746, CLIA, CAP, NYSDOH, ISO 22716, ISO 22000, NSF-2, NSF-3, FDA CFR Part 210, 211, 600',
+        'Procedures, Forms and Process for Regulatory Strategy and Submissions, ensuring compliance with EU, FDA, ROW, ISO 14971 risk analysis, ISO 62366 usability studies, ISO 62304 software compliance,, Analytical and Clinical Study',
+        'Procedures, Forms and Process for Post Market Surveillance, Vigilance, Complaints, and Field Actions',
       ],
       link: '/info/products',
     },
     {
       header: 'OUR CLIENTS',
       description:
-        'At Antrix, we have worked with a number of clients in Medical Device, IVD, LDT, SaMD Industries.',
+        'At Antrix, we have worked with a number of clients in Medical Device, IVD, LDT, SaMD, and digital health Industries',
       list: [
-        'Our clients trust us for our expert regulatory affairs, clinical affairs, and quality assurance consulting services and products.',
-        'We believe in delivering quality and timely services to our clients to ensure their success in the market and assist in innovation and growth. Recognize revenue faster.',
+        'Our clients trust us for our expert regulatory, clinical, and quality assurance consulting services and products.',
+        'We believe in delivering high quality and timely services to our clients to ensure their success in the market and assist in innovation and growth. Purchase of product solutions enables accelerated implementation allowing faster revenue recognition.',
         'Our clients range from small startups to large multinational corporations. We have helped our clients achieve their goals and improve patient safety and effectiveness outcomes.',
       ],
       link: '',
